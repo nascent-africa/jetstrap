4cb6955946c1793d6a7598e4a1e171a94c3b618c
tsommie <anitchec.dev@gmail.com>
2020-09-20 09:24:47 +0000

    Fix page headers.

--- stubs/livewire/resources/views/api/index.blade.php
+++ stubs/livewire/resources/views/api/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="h4 font-weight-bold">
             API Tokens
         </h2>
     </x-slot>
