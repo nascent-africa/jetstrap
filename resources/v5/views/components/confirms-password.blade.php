bcba508a4efc88d1e05054cd33011304606c6c1f
tsommie <anitchec.dev@gmail.com>
2020-09-24 11:50:46 +0000

    Add password confirmation.

---
+++ resources/v5/views/components/confirms-password.blade.php
@@ -0,0 +1,65 @@
+@props(['title' => __('Confirm Password'), 'content' => __('For your security, please confirm your password to continue.'), 'button' => __('Confirm')])
+
+@php
+    $confirmableId = md5($attributes->wire('then'));
+@endphp
+
+<span
+    {{ $attributes->wire('then') }}
+    x-data
+    x-ref="span"
+    wire:click="$emit('startConfirmingPassword', '{{ $confirmableId }}')"
+    x-on:password-confirmed.window="setTimeout(() => $event.detail.id === '{{ $confirmableId }}' && $refs.span.dispatchEvent(new CustomEvent('then', { bubbles: false })), 250);"
+>
+    {{ $slot }}
+</span>
+
+@once
+<x-jet-dialog-modal id="confirmingPasswordModal">
+    <x-slot name="title">
+        {{ $title }}
+    </x-slot>
+
+    <x-slot name="content">
+        {{ $content }}
+
+        <div class="mt-4" x-data="{}" x-on:confirming-password.window="setTimeout(() => $refs.confirmable_password.focus(), 250)">
+            <x-jet-input type="password" class="{{ $errors->has('confirmable_password') ? 'is-invalid' : '' }}" placeholder="Password"
+                         x-ref="confirmable_password"
+                         wire:model.defer="confirmablePassword"
+                         wire:keydown.enter="confirmPassword" />
+
+            <x-jet-input-error for="confirmable_password" />
+        </div>
+    </x-slot>
+
+    <x-slot name="footer">
+        <x-jet-secondary-button wire:click="stopConfirmingPassword" wire:loading.attr="disabled"
+                                data-dismiss="modal">
+            Nevermind
+        </x-jet-secondary-button>
+
+        <x-jet-button class="ml-2" wire:click="confirmPassword" wire:loading.attr="disabled">
+            {{ $button }}
+        </x-jet-button>
+    </x-slot>
+</x-jet-dialog-modal>
+@endonce
+
+@push('scripts')
+    <script>
+        let modal = new Bootstrap.Modal(document.getElementById('confirmingPasswordModal'))
+
+        window.addEventListener('confirming-password', event => {
+            modal.toggle()
+        })
+        window.addEventListener('password-confirmed', event => {
+            modal.hide()
+        })
+
+        Livewire.on('startConfirmingPassword', (id) => {
+            @this.startConfirmingPassword(id)
+            modal.hide()
+        })
+    </script>
+@endpush
