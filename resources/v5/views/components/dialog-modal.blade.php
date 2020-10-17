422ba4b28427c86e2b494bef2ec67263ef5486e9
tsommie <anitchec.dev@gmail.com>
2020-10-17 23:44:55 +0000

    Fix close button

--- resources/v5/views/components/dialog-modal.blade.php
+++ resources/v5/views/components/dialog-modal.blade.php
@@ -4,9 +4,8 @@
     <div class="modal-content">
         <div class="modal-header">
             <h5 class="modal-title">{{ $title }}</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
+            <!-- Modal close button is different in bootstrap 5 -->
+            <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
             {{ $content }}
