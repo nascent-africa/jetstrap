e25ba8c3331a6e195076f36d370ebfd6380eba4e
phamthao <phamthao03596@gmail.com>
2021-01-11 10:57:24 +0700

    Uppercase button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'btn btn-dark']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'btn btn-dark text-uppercase']) }}>
     {{ $slot }}
 </button>
