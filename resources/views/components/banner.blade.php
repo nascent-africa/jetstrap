851d0c3267ea5bbcf81a08c1303aec2f81ae1256
vanthao03596 <34786441+vanthao03596@users.noreply.github.com>
2021-01-29 16:32:40 +0700

    Style missing banner component

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,7 +1,7 @@
 @props(['style' => session('flash.bannerStyle', 'success'), 'message' => session('flash.banner')])
 
 <div x-data="{{ json_encode(['show' => true, 'style' => $style, 'message' => $message]) }}"
-            :class="{ 'bg-indigo-500': style == 'success', 'bg-red-700': style == 'danger' }"
+            :class="{ 'bg-indigo': style == 'success', 'bg-danger': style == 'danger' }"
             x-show="show && message"
             x-init="
                 document.addEventListener('banner-message', event => {
@@ -10,26 +10,25 @@
                     show = true;
                 });
             ">
-    <div class="max-w-screen-xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between flex-wrap">
-            <div class="w-0 flex-1 flex items-center min-w-0">
-                <span class="flex p-2 rounded-lg" :class="{ 'bg-indigo-600': style == 'success', 'bg-red-600': style == 'danger' }">
-                    <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <div class="container mx-auto py-2 px-md-2 px-sm-3">
+        <div class="d-flex align-items-center justify-content-between flex-wrap">
+            <div style="flex: 1 1 0%;" class="d-flex align-items-center">
+                <span class="d-flex p-2 rounded-lg" :class="{ 'bg-indigo': style == 'success', 'bg-danger': style == 'danger' }">
+                    <svg style="width: 1.25rem; height: 1.25rem" class="text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                 </span>
 
-                <p class="ml-3 font-medium text-sm text-white truncate" x-text="message"></p>
+                <p class="m-0 ml-3 font-weight-normal text-white" x-text="message"></p>
             </div>
 
-            <div class="flex-shrink-0 sm:ml-3">
+            <div class="flex-shrink-0">
                 <button
                     type="button"
-                    class="-mr-1 flex p-2 rounded-md focus:outline-none sm:-mr-2 transition ease-in-out duration-150"
-                    :class="{ 'hover:bg-indigo-600 focus:bg-indigo-600': style == 'success', 'hover:bg-red-600 focus:bg-red-600': style == 'danger' }"
+                    class="btn btn-link d-flex p-2 rounded-md"
                     aria-label="Dismiss"
                     x-on:click="show = false">
-                    <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg style="width: 1.25rem; height: 1.25rem" class="text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
