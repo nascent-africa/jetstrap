8d09bd69dcc82a79c18f19a323a7a30f2fdc37ef
tsommie <anitchec.dev@gmail.com>
2021-04-04 12:09:31 +0000

    2 spaces indentation

--- stubs/inertia/resources/js/Jetstream/SectionTitle.vue
+++ stubs/inertia/resources/js/Jetstream/SectionTitle.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="px-4 px-sm-0">
-        <div class="d-flex justify-content-between">
-            <div>
-                <h3 class="h5">
-                    <slot name="title"></slot>
-                </h3>
+	<div class="px-4 px-sm-0">
+		<div class="d-flex justify-content-between">
+			<div>
+				<h3 class="h5">
+					<slot name="title"></slot>
+				</h3>
 
-                <p class="mt-1 text-muted">
-                    <slot name="description"></slot>
-                </p>
-            </div>
+				<p class="mt-1 text-muted">
+					<slot name="description"></slot>
+				</p>
+			</div>
 
-            <div>
-                <slot name="aside"></slot>
-            </div>
-        </div>
-    </div>
+			<div>
+				<slot name="aside"></slot>
+			</div>
+		</div>
+	</div>
 </template>
